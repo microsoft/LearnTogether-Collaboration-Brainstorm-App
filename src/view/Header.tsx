ba12e3aa1ea33a07f564f739de0e9aad64858d7c
Dan Wahlin <dwahlin@xmlforasp.net>
2021-08-26 21:53:47 -0700

    Refactor util functions

--- src/view/Header.tsx
+++ src/view/Header.tsx
@@ -10,14 +10,8 @@ import { DefaultColor } from "./Color";
 import { ColorPicker } from "./ColorPicker";
 import { NoteData } from "../Types";
 import { NOTE_SIZE } from "./Note.style";
+import { uuidv4 } from '../utils';
 
-function uuidv4() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    var r = (Math.random() * 16) | 0,
-      v = c === "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-}
 export interface HeaderProps {
   model: BrainstormModel;
   author: AzureMember;
