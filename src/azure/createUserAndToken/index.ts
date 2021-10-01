a97aa5657ff68118badb6c3c89c805defb7751dd
Reza Jooyandeh <rejooyan@microsoft.com>
2021-10-01 13:02:27 -0700

    simplify azure functions

--- src/azure/createUserAndToken/index.ts
+++ src/azure/createUserAndToken/index.ts
@@ -2,8 +2,13 @@ import { CommunicationIdentityClient } from "@azure/communication-identity";
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { getResourceConnectionString } from "../lib/envHelper";
 
+// Create a user on demand and issue an Azure Communication Services token for chatting.
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const userInfo = await new CommunicationIdentityClient(getResourceConnectionString()).createUserAndToken(['chat']);
+    // Initialize a communication identity client
+    const identityClient = new CommunicationIdentityClient(getResourceConnectionString());
+
+    // Create a user and token
+    const userInfo = await identityClient.createUserAndToken(['chat']);
 
     context.res = {
         body: { ...userInfo }
--- src/azure/joinThread/index.ts
+++ src/azure/joinThread/index.ts
@@ -1,18 +1,35 @@
-import { CommunicationUserToken } from "@azure/communication-identity";
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { joinThread } from "../lib/chat/moderator";
+import { CommunicationUserToken } from "@azure/communication-identity";
+import { AzureCommunicationTokenCredential } from '@azure/communication-common';
+import { ChatClient } from '@azure/communication-chat';
+import { getResourceEndPoint } from '../lib/envHelper';
 
+// Add a user to a chat thread
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const moderatorEntity: CommunicationUserToken =JSON.parse(context.bindings.moderatorEntity?.value);
-
+    // Extract the user and chat thread information from the request
     const {threadId, userId, displayName} = req.query;
 
-    await joinThread(userId, displayName, moderatorEntity.user.communicationUserId, threadId);
+    // Get the chat thread moderator's user information from the storage 
+    const chatThreadModerator: CommunicationUserToken =
+      JSON.parse(context.bindings.moderatorEntity?.value);
+    
+    // Initialize a chat client
+    const chatClient = new ChatClient(
+      getResourceEndPoint(),
+      new AzureCommunicationTokenCredential(chatThreadModerator.token));
+    
+    // Initialize a client for the chat thread
+    const chatThreadClient = chatClient.getChatThreadClient(threadId);
 
-    context.res = {
-        body: `Joined threadId: ${threadId}`
-    };
+    // Add the new user to the chat thread.
+    await chatThreadClient.addParticipants({
+      participants: [ {
+        id: { communicationUserId: userId },
+        displayName: displayName
+      }]
+    });
 
+    context.res = { body: `Joined threadId: ${threadId}` };
 };
 
 export default httpTrigger;
